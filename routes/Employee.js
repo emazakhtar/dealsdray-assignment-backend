a96644d8744f0cdbd5e6ca4c8482a81096192549
Emaz <emazakhtar11@gmail.com>
2024-10-03 03:32:19 +0530

    .

--- routes/Employee.js
+++ routes/Employee.js
@@ -1,25 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controllers/Employee");
-const multer = require("multer");
 
 // Multer setup for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Directory where files will be stored
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-const upload = multer({ storage: storage });
 
 router
-  .post("/create-employee", upload.single("image"), employeeController.create)
+  .post("/create-employee", employeeController.create)
   .patch("/update-employee/:id", employeeController.update)
   .get("/get-employees", employeeController.getAll)
   .get("/get-employee/:id", employeeController.getEmployeeById)
